3eb94cfabb73183c11c4ae25e1bcd88e4aa591ce
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-12-06 00:36:42 -0800

    push

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./keys").mongoURI;
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
+const User = require("./model/User");
+const bodyParser = require("body-parser");
+const passport = require('passport');
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-});
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.port || 5000;
 
