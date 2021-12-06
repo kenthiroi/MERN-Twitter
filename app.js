5099bf7f88b32e5de8f94f711bfaf4346c163ea8
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-12-05 20:04:23 -0800

    First push

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+});
+
+const port = process.env.port || 5000;
+
+app.listen(port, ()=> {console.log(`Listening on port ${port}`)});
